77e96f65b1f351f49688070c4c282fc61b56b244
dhq_boiler <dhq_boiler@live.jp>
2023-11-21 00:13:18 +0900

    Revert "リファクタリング"
    This reverts commit db90b970df839654a8e607567d4d9c9a7783d48e.

--- boilersExtensions/NamespaceRenamer.cs
+++ boilersExtensions/NamespaceRenamer.cs
@@ -12,37 +12,39 @@ namespace boilersExtensions
     {
         public static async Task RenameNamespaceAsync(string solutionPath, string oldNamespace, string newNamespace)
         {
-            using var workspace = MSBuildWorkspace.Create();
-            var solution = await workspace.OpenSolutionAsync(solutionPath);
-
-            foreach (var projectId in solution.ProjectIds)
+            using (var workspace = MSBuildWorkspace.Create())
             {
-                var project = solution.GetProject(projectId);
-                foreach (var documentId in project.DocumentIds)
+                var solution = await workspace.OpenSolutionAsync(solutionPath);
+
+                foreach (var projectId in solution.ProjectIds)
                 {
-                    var document = project.GetDocument(documentId);
-                    var syntaxRoot = await document.GetSyntaxRootAsync();
-
-                    var namespaceDeclarations = syntaxRoot.DescendantNodes()
-                        .OfType<NamespaceDeclarationSyntax>()
-                        .Where(nd => nd.Name.ToString() == oldNamespace);
-
-                    foreach (var namespaceDeclaration in namespaceDeclarations)
+                    var project = solution.GetProject(projectId);
+                    foreach (var documentId in project.DocumentIds)
                     {
-                        var newNamespaceDeclaration =
-                            namespaceDeclaration.WithName(SyntaxFactory.ParseName(newNamespace));
-                        syntaxRoot = syntaxRoot.ReplaceNode(namespaceDeclaration, newNamespaceDeclaration);
+                        var document = project.GetDocument(documentId);
+                        var syntaxRoot = await document.GetSyntaxRootAsync();
+
+                        var namespaceDeclarations = syntaxRoot.DescendantNodes()
+                            .OfType<NamespaceDeclarationSyntax>()
+                            .Where(nd => nd.Name.ToString() == oldNamespace);
+
+                        foreach (var namespaceDeclaration in namespaceDeclarations)
+                        {
+                            var newNamespaceDeclaration =
+                                namespaceDeclaration.WithName(SyntaxFactory.ParseName(newNamespace));
+                            syntaxRoot = syntaxRoot.ReplaceNode(namespaceDeclaration, newNamespaceDeclaration);
+                        }
+
+                        var formattedRoot = Formatter.Format(syntaxRoot, workspace);
+                        var newDocument = document.WithSyntaxRoot(formattedRoot);
+                        project = newDocument.Project;
                     }
 
-                    var formattedRoot = Formatter.Format(syntaxRoot, workspace);
-                    var newDocument = document.WithSyntaxRoot(formattedRoot);
-                    project = newDocument.Project;
+                    solution = project.Solution;
                 }
 
-                solution = project.Solution;
+                workspace.TryApplyChanges(solution);
             }
-
-            workspace.TryApplyChanges(solution);
         }
     }
 }
